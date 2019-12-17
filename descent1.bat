ea7e6d7e5f57e5580fa70e18eec87ec703f736e5
Davyd McColl <davydm@gmail.com>
2019-12-17 13:59:37 +0200

    :truck: move different versions into their own folders -- they will stomp over each other's player & save data otherwise

---
+++ descent1.bat
@@ -0,0 +1,3 @@
+@echo off
+set DESCENT_HOME=%~dp0\d1
+"%DESCENT_HOME%\d1x-rebirth.exe" -hogdir "%DESCENT_HOME%\data"
---
+++ descent2.bat
@@ -0,0 +1,3 @@
+@echo off
+set DESCENT_HOME=%~dp0\d2
+"%DESCENT_HOME%\d2x-rebirth.exe" -hogdir "%DESCENT_HOME%\data"
